f8df94b983028ab72a8aa2745f302083beb9fc23
RamyaSprakash <ramyaprakash375@gmail.com>
2025-02-18 17:23:09 +0530

    fff

---
+++ src/programming/Life.java
@@ -0,0 +1,10 @@
+package programming;
+
+public class Life {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("aramya");
+	}
+
+}
--- src/programming/Str.java
+++ src/programming/Str.java
@@ -3,24 +3,6 @@ package programming;
 public class Str {
 
 	public static void main(String[] args) {
-		 String name="Ramya";
-	        int c;
-	        char[] ch=name.toCharArray();
-	       
-	        for(int i=0;i<ch.length;i++)
-	        {
-	        	 c=0;
-	        	  for(int j=i+1;j<ch.length;j++)
-	        	  {
-	        		  if(ch[i]==ch[j]  && ch[j]!=' ')
-	        		  {
-	        			  c++;
-	        		  }
-	        	  }
-	        	  if(c>0)
-	              {
-	              	System.out.println(ch[i]);
-	              }  
-	        }
+		System.out.println("Ramya");
 	}
 }
